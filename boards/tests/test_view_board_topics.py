24e0f00bde62e89cb6ebae385d818e7dbb11f695
ehsan javanmard <javanmard.ehsan@gmail.com>
2019-08-02 01:19:11 +0430

    first commit

---
+++ boards/tests/test_view_board_topics.py
@@ -0,0 +1,29 @@
+from django.urls import reverse ,resolve
+from django.test import TestCase
+from ..views import TopicListView, new_topic
+from ..models import Board, Topic, Post, User
+from ..forms import NewTopicForm
+
+class BoardTopicsTests(TestCase):
+    def setUp(self):
+        Board.objects.create(name='Django', description='Django board.')
+
+    def test_board_topics_view_success_status_code(self):
+        url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_board_topics_view_not_found_status_code(self):
+        url = reverse('board_topics', kwargs={'pk': 99})
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 404)
+
+    def test_board_topics_url_resolves_board_topics_view(self):
+        view = resolve('/boards/1/')
+        self.assertEquals(view.func.view_class, TopicListView)
+
+    def test_board_topics_view_contains_link_back_to_homepage(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        response = self.client.get(board_topics_url)
+        homepage_url = reverse('home')
+        self.assertContains(response, 'href="{0}"'.format(homepage_url))
