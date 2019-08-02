a8a78858f71e49c6bbb9c10ae444af26bebc1816
ehsan javanmard <javanmard.ehsan@gmail.com>
2019-08-02 08:06:23 +0430

    email settings

--- forum/settings.py
+++ forum/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
 
 LOGOUT_REDIRECT_URL = 'home'
 LOGIN_REDIRECT_URL = 'home'
